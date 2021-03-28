da8c8e1bc0cddc2bcd1308a1a894bf201796188d
Dreamkeeper <jinhan@umich.edu>
2021-03-28 23:46:36 +0800

    mini-katago

--- kata_mcts.py
+++ kata_mcts.py
@@ -199,7 +199,7 @@ class MCTSNode(object):
             new_position = self.position.play_move(
                 coords.from_flat(fcoord))
             new_game_state = self.game_state.play_move(
-                coors.to_gtp(coords.from_flat(fcoord)))
+                coords.to_gtp(coords.from_flat(fcoord)))
             self.children[fcoord] = MCTSNode(
                 new_position, new_game_state, fmove=fcoord, parent=self)
         return self.children[fcoord]
